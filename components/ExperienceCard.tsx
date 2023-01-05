422cf97d43424499981fef300fb19fadd1ba31b3
KT 2022 <103476893+kt946@users.noreply.github.com>
2023-01-04 23:23:50 -0500

    added Experience section

---
+++ components/ExperienceCard.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+type Props = {};
+
+export default function ExperienceCard(props: Props) {
+  return <div>ExperienceCard</div>;
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,11 @@ import Head from 'next/head';
 import About from '../components/About';
 import Header from '../components/Header';
 import Hero from '../components/Hero';
+import WorkExperience from '../components/WorkExperience';
 
 export default function Home() {
   return (
-    <div className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0'>
+    <div className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0 scroll-smooth'>
       <Head>
         <title>Kyle's Portfolio</title>
       </Head>
@@ -21,6 +22,9 @@ export default function Home() {
       </section>
 
       {/* Experience */}
+      <section id='experience' className='snap-center'>
+        <WorkExperience />
+      </section>
 
       {/* Skills */}
 
