10755a4df64e2f9865882901d0f3e762d45bac06
KT 2022 <103476893+kt946@users.noreply.github.com>
2023-01-05 17:53:22 -0500

    updated scrollbar styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Link from 'next/link';
 import About from '../components/About';
 import ContactMe from '../components/ContactMe';
 import Header from '../components/Header';
@@ -9,7 +10,10 @@ import WorkExperience from '../components/WorkExperience';
 
 export default function Home() {
   return (
-    <div className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scroll-smooth'>
+    <div 
+      className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scroll-smooth
+      scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80'
+    >
       <Head>
         <title>Kyle's Portfolio</title>
       </Head>
@@ -39,6 +43,18 @@ export default function Home() {
       <section id='contact' className='snap-center'>
         <ContactMe />
       </section>
+
+      <Link href='#hero'>
+        <footer className='sticky bottom-5 w-full cursor-pointer'>
+          <div className='flex items-center justify-center'>
+            <img 
+              className='h-10 w-10 rounded-full filter grayscale hover:grayscale-0 cursor-pointer'
+              src="https://kt946.github.io/my-portfolio-react/static/media/mylogo.03f1ddde85ac76243c4bf8d9e0fec6ea.svg" 
+              alt="" 
+            />
+          </div>
+        </footer>
+      </Link>
     </div>
   );
 }
