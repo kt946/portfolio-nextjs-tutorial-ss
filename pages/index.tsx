cf5b2f2f9b7558e320fbfd386203e3273a896d99
KT 2022 <103476893+kt946@users.noreply.github.com>
2023-01-04 22:28:27 -0500

    added Header and Hero components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,20 @@
 import Head from 'next/head';
+import Header from '../components/Header';
+import Hero from '../components/Hero';
 
 export default function Home() {
   return (
-    <div>
+    <div className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0'>
       <Head>
-        {/* eslint-disable-next-line react/no-unescaped-entities */}
         <title>Kyle's Portfolio</title>
       </Head>
 
-      {/* Header */}
+      <Header />
 
       {/* Hero */}
+      <section id='hero' className='snap-center'>
+        <Hero />
+      </section>
 
       {/* About */}
 
