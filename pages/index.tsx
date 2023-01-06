052bef58db56055381ca4d580ae3b535278c14f8
KT 2022 <103476893+kt946@users.noreply.github.com>
2023-01-06 15:33:13 -0500

    added sanity types, util functions, api functions, and connected Sanity to frontend

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import type { GetStaticProps } from 'next';
 import Head from 'next/head';
 import Link from 'next/link';
 import About from '../components/About';
@@ -7,8 +8,22 @@ import Hero from '../components/Hero';
 import Projects from '../components/Projects';
 import Skills from '../components/Skills';
 import WorkExperience from '../components/WorkExperience';
+import { Experience, PageInfo, Project, Skill, Social } from '../typings';
+import { fetchExperiences } from '../utils/fetchExperiences';
+import { fetchPageInfo } from '../utils/fetchPageInfo';
+import { fetchProjects } from '../utils/fetchProjects';
+import { fetchSkills } from '../utils/fetchSkills';
+import { fetchSocials } from '../utils/fetchSocials';
 
-export default function Home() {
+type Props = {
+  pageInfo: PageInfo;
+  experiences: Experience[];
+  skills: Skill[];
+  projects: Project[];
+  socials: Social[];
+}
+
+const Home = ({ pageInfo, experiences, projects, skills, socials }: Props) => {
   return (
     <div 
       className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scroll-smooth scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80'
@@ -17,7 +32,7 @@ export default function Home() {
         <title>Kyle's Portfolio</title>
       </Head>
 
-      <Header />
+      <Header socials={socials}/>
 
       <section id='hero' className='snap-center'>
         <Hero />
@@ -57,3 +72,28 @@ export default function Home() {
     </div>
   );
 }
+
+export default Home;
+
+export const getStaticProps: GetStaticProps<Props> = async () => {
+  const pageInfo: PageInfo = await fetchPageInfo();
+  const experiences: Experience[] = await fetchExperiences();
+  const skills: Skill[] = await fetchSkills();
+  const projects: Project[] = await fetchProjects();
+  const socials: Social[] = await fetchSocials();
+
+  return {
+    props: {
+      pageInfo, 
+      experiences, 
+      projects, 
+      skills, 
+      socials
+    },
+
+    // Next.js will attempt to re-generate the page:
+    // - When a request comes in
+    // - At most once every 10 seconds
+    revalidate: 10,
+  };
+};
