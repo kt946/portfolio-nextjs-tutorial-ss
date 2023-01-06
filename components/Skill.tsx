a6b989f387f9e46db805c2d060fd42817a6f30c7
KT 2022 <103476893+kt946@users.noreply.github.com>
2023-01-06 16:32:05 -0500

    updated Skills with sanity backend

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -1,21 +1,24 @@
 import React from 'react';
 import { motion } from 'framer-motion';
+import { Skill as SkillType } from '../typings';
+import { urlFor } from '../sanity';
 
 type Props = {
+  skill: SkillType;
   directionLeft?: boolean;
 };
 
-function Skill({ directionLeft }: Props) {
+function Skill({ skill, directionLeft }: Props) {
   return (
     <div className='group relative flex cursor-pointer'>
       <motion.img
         initial={{
           x: directionLeft ? -200 : 200,
-          opacity: 0,
+          opacity: 0, 
         }}
         transition={{ duration: 1 }}
         whileInView={{ opacity: 1, x: 0 }}
-        src='https://cdn.sanity.io/images/djq1iaeq/production/6ed24a12e20d2560228d57a576ee4b2c6c7acda8-512x512.png'
+        src={urlFor(skill?.image).url()}
         className='rounded-full border border-gray-500 object-cover w-24 h-24 md:w-28 md:h-28 
         xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out'
       />
@@ -24,7 +27,7 @@ function Skill({ directionLeft }: Props) {
       group-hover:bg-white h-24 w-24 md:w-28 md:h-28 xl:w-32 xl:h-32 rounded-full z-0'
       >
         <div className='flex items-center justify-center h-full'>
-          <p className='text-3xl font-bold text-black opacity-100'>100%</p>
+          <p className='text-3xl font-bold text-black opacity-100'>{skill.progress}%</p>
         </div>
       </div>
     </div>
