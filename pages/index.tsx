873d757c1da9bf981e1cc1bc414ff05a9d8b9415
KT 2022 <103476893+kt946@users.noreply.github.com>
2023-01-06 16:11:46 -0500

    added sanity backend to experience section and cards

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,57 +21,73 @@ type Props = {
   skills: Skill[];
   projects: Project[];
   socials: Social[];
-}
+};
 
 const Home = ({ pageInfo, experiences, projects, skills, socials }: Props) => {
   return (
-    <div 
-      className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scroll-smooth scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80'
-    >
+    <div className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scroll-smooth scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80'>
       <Head>
         <title>Kyle's Portfolio</title>
       </Head>
 
-      <Header socials={socials}/>
+      <Header socials={socials} />
 
-      <section id='hero' className='snap-center'>
+      <section
+        id='hero'
+        className='snap-center'
+      >
         <Hero pageInfo={pageInfo} />
       </section>
 
-      <section id='about' className='snap-center'>
+      <section
+        id='about'
+        className='snap-center'
+      >
         <About pageInfo={pageInfo} />
       </section>
 
-      <section id='experience' className='snap-center'>
-        <WorkExperience />
+      <section
+        id='experience'
+        className='snap-center'
+      >
+        <WorkExperience experiences={experiences} />
       </section>
 
-      <section id='skills' className='snap-center'>
+      <section
+        id='skills'
+        className='snap-center'
+      >
         <Skills />
       </section>
 
-      <section id='projects' className='snap-center'>
+      <section
+        id='projects'
+        className='snap-center'
+      >
         <Projects />
       </section>
 
-      <section id='contact' className='snap-center'>
+      <section
+        id='contact'
+        className='snap-center'
+      >
         <ContactMe />
       </section>
 
       <Link href='#hero'>
         <footer className='sticky bottom-5 w-full cursor-pointer'>
           <div className='flex items-center justify-center'>
-            <img 
+            <img
               className='h-10 w-10 rounded-full filter grayscale hover:grayscale-0 cursor-pointer'
-              src="https://kt946.github.io/my-portfolio-react/static/media/mylogo.03f1ddde85ac76243c4bf8d9e0fec6ea.svg" 
-              alt="" 
+              src='https://kt946.github.io/my-portfolio-react/static/media/mylogo.03f1ddde85ac76243c4bf8d9e0fec6ea.svg'
+              alt=''
             />
           </div>
         </footer>
       </Link>
     </div>
   );
-}
+};
 
 export default Home;
 
@@ -84,11 +100,11 @@ export const getStaticProps: GetStaticProps<Props> = async () => {
 
   return {
     props: {
-      pageInfo, 
-      experiences, 
-      projects, 
-      skills, 
-      socials
+      pageInfo,
+      experiences,
+      projects,
+      skills,
+      socials,
     },
 
     // Next.js will attempt to re-generate the page:
